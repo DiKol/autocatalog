4cfa3490600188c0287fba6888743512464ca45a
DiKol <dmytrokolesnyk@gmail.com>
2022-06-03 11:56:44 +0300

    overview

--- Client/catalog/src/app/features/overview/components/overview.component.ts
+++ Client/catalog/src/app/features/overview/components/overview.component.ts
@@ -11,8 +11,11 @@ import { ModelDataService } from '../../model/service/model-data.service';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
 import { NestedTreeControl } from '@angular/cdk/tree';
 import { BrandDataService } from '../../brand/service/brand-data.service';
+import { Car } from 'src/app/core/model/car';
+import { CarDataService } from '../../car/service/car-data.service';
 
 interface TNode {
+  id: number;
   name: string;
   children?: TNode[];
 }
@@ -29,45 +32,56 @@ export class OverviewComponent implements AfterViewInit, OnInit {
   treeControl = new NestedTreeControl<TNode>(node => node.children);
   dataSource = new MatTreeNestedDataSource<TNode>();
 
-  //models$: Observable<Model[]>;
+  cars: Car[] =[];
+  flexstyle: { [klass: string]: any; } = {};
+  count: number = 8;
+  @ViewChild('xrow') xrow!: ElementRef;
 
   constructor(
     public httpClient: HttpClient,
     public modelService: ModelDataService,
-    public brandService: BrandDataService
+    public brandService: BrandDataService,
+    public carService: CarDataService,
   ) {
+
+    this.carService.getCars().subscribe(cars=>{
+
+      for (var i = 0; i < cars.length; i++)
+      {
+        this.cars.push(cars[i]);
+      }
+
+    });
 
 
     let TREE_DATA: TNode[] = [];
 
-    brandService.getBrands().subscribe(brands=>
-      {
-        modelService.getModels().subscribe(models=>
-          {
-            brands.forEach(elem => {
-              //elem.id
-              var children : TNode[] = [];
-              var childs = models.filter(x=>x.brand.id == elem.id);
-              childs.forEach(c => {
+    brandService.getBrands().subscribe(brands => {
+      modelService.getModels().subscribe(models => {
+        brands.forEach(elem => {
+          var children: TNode[] = [];
+          var childs = models.filter(x => x.brand.id == elem.id);
+          childs.forEach(c => {
 
-                let cnode: TNode = {
-                  name: c.name
-                };
-                children.push(cnode);
-               });
-
-               let node: TNode = {
-                name: elem.name,
-                children : childs
-              };
-              TREE_DATA.push(node);
-
-
+            let cnode: TNode = {
+              id: c.id,
+              name: c.name
+            };
+            children.push(cnode);
           });
-          this.dataSource.data = TREE_DATA;
+
+          let node: TNode = {
+            id: elem.id,
+            name: elem.name,
+            children: childs
+          };
+          TREE_DATA.push(node);
+
+
+        });
+        this.dataSource.data = TREE_DATA;
 
       });
-
     });
 
   }
@@ -75,14 +89,15 @@ export class OverviewComponent implements AfterViewInit, OnInit {
   hasChild = (_: number, node: TNode) => !!node.children && node.children.length > 0;
 
 
+  filterBrand(id:number){}
+  filterModel(id:number){}
 
   ngAfterViewInit() {
-
+    var w = this.xrow.nativeElement.offsetWidth/5-1;
+    this.flexstyle = { flex: '1 0 ' + w + 'px' };
   }
 
   ngOnInit() {
-
   }
-
 
 }
